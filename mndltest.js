a6fb37c3fa8d49ccd464aec193e6c14414c69987
jchansard <josh.chansard@gmail.com>
2016-03-28 08:12:56 -0500

    rewrite for node

--- mndltest.js
+++ mndltest.js
@@ -2,31 +2,33 @@
  * mndltest.js
  *
  * basic unit test framework. supports assert.isTrue and assert.equals. To run tests,
- * call mndltest.runTests(tests), where tests is a simple object with optional setup
+ * call tests.run(testCases), where testCases is a simple object with optional setup
  * and teardown properties, as well as test case properties. For example:
  *
  * test = {
  * 		setup: function() { 
- *			this.fixture = new Fixture();
+ *			test.fixture = new Fixture();
  *		},
  *		teardown: function() {
- *			this.fixture = null;
+ *			test.fixture = null;
  *		},
  *		"fixture property is not null": function() {
- *			assert.isTrue(fixture.property !== null);
+ *			test.isTrue(fixture.property !== null);
  *		}
  * }
  * -------------
  * Josh Chansard 
  * https://github.com/jchansard/mndltest
  */
-(function() {
+module.exports = function($, id, container) {
+
 	// css relies on it being div#mndltest-results
-	const TEST_CONTAINER_ID = 'mndltest-results'
-	const TEST_CONTAINER = 'div#' + TEST_CONTAINER_ID;
+	const TEST_CONTAINER_ID = id || 'mndltest-results'
+	const TEST_CONTAINER = container || 'div#' + TEST_CONTAINER_ID;
+	var mndltest = {};
 
 	// assertions; throws an AssertionError on fail
-	this.assert = {
+	mndltest.assert = {
 		// actual must be true
 		isTrue: function(actual)
 		{
@@ -131,132 +133,137 @@
 		}
 	};
 
-	this.mndltest = {
-		// current group of test cases
-		_$currentList: undefined,
+	if ($)
+	{
+		mndltest.tests = {
+			// current group of test cases
+			_$currentList: undefined,
 
-		// create elements to house results
-		_start: function(testGroupName)
-		{
-			if ($(TEST_CONTAINER).length == 0)
+			// create elements to house results
+			_start: function(testGroupName)
 			{
-				$('body').append($(document.createElement('div')).attr('id',TEST_CONTAINER_ID));
-				$(TEST_CONTAINER).click(function() { $(this).hide(200); });
-			}
-	
-			var $div = $(document.createElement('div')).addClass('test-group pass').appendTo(TEST_CONTAINER)
-				.append($(document.createElement('h2')).text(testGroupName));
-			this._$currentList = $(document.createElement('ul')).appendTo($div);;
-		},
-
-		// cleanup
-		_end: function()
-		{
-			this._$currentList = undefined;
-		},
-
-		// try fn, catching errors. if error, display fail message. 
-		// otherwise, display pass message, unless suppressPass=true.
-		_tryToDo: function(fn, message, suppressPass)
-		{
-			suppressPass = suppressPass || false;
-			if (typeof fn === 'function')
-			{
-				try { 
-					fn();
-					if (!suppressPass) { this._addResult('pass', message,  fn.toString()); }
-				}
-				catch (e) {
-					this._addResult('fail', message, fn.toString(), e);
-					console.error(e)
-				}
-			}
-		},
-
-		// add a result <li>
-		_addResult: function(result, description, functionToString, error)
-		{
-			var $testResult = $(document.createElement('li')).click(functionToString, this._displayTestFunction);
-			$testResult = $testResult.addClass(result).html(description);
-			if (error != undefined)
-			{
-				$(document.createElement('ul'))
-					.append($(document.createElement('li')).html(error.name + ': ' + error.message))
-					.appendTo($testResult);
-			}
-			if (result === 'fail') { this._$currentList.parents('div.test-group').addClass('fail').removeClass('pass'); }
-			this._$currentList.append($testResult);
-		},
-
-		// display results. 
-		_displayResults: function()
-		{
-			$(TEST_CONTAINER).show(200);
-		},
-
-		// displays a test case's test function
-		_displayTestFunction: function(e) 
-		{
-			e.stopPropagation();
-
-			if ($(this).data('preCreated'))	{ 
-				$(this).children('pre').toggle(300);
-			}
-
-			if ($(this).data('preCreated') === undefined)
-			{
-				var funcDisplayHandler = function(e) {
-					e.stopPropagation();
-					$(this).children('pre').hide(300);
-				}.bind(this);
-
-				$(this).data('preCreated', true);
-				$(document.createElement('pre'))
-				.html(e.data)
-				.click(funcDisplayHandler)
-				.hide()
-				.appendTo($(this))
-				.show(300);
-			}
-		},
-
-		dispatchEvent: function(type, properties, target)
-		{
-			var e = $.Event(type, properties);
-			$(target).trigger(e);
-		},
-
-		// run tests
-		runTests: function(testCases)
-		{
-			for (thisCase in testCases)
-			{
-				mndltest._start(thisCase);
-				
-				var testCase = testCases[thisCase];
-				var fns = Object.keys(testCase);
-				var setup, teardown;
-				var tests = [];
-				var descriptions = [];
-				var i;
-				for (var i = 0; i < fns.length; i++)
+				if ($(TEST_CONTAINER).length == 0)
 				{
-					if (fns[i] === 'setup') { setup = testCase[fns[i]]; }
-					else if (fns[i] === 'teardown') { teardown = testCase[fns[i]]; }
-					else {
-						descriptions.push(fns[i]);
-						tests.push(testCase[fns[i]]);
+					$('body').append($(document.createElement('div')).attr('id',TEST_CONTAINER_ID));
+					$(TEST_CONTAINER).click(function() { $(this).hide(200); });
+				}
+		
+				var $div = $(document.createElement('div')).addClass('test-group pass').appendTo(TEST_CONTAINER)
+					.append($(document.createElement('h2')).text(testGroupName));
+				this._$currentList = $(document.createElement('ul')).appendTo($div);;
+			},
+
+			// cleanup
+			_end: function()
+			{
+				this._$currentList = undefined;
+			},
+
+			// try fn, catching errors. if error, display fail message. 
+			// otherwise, display pass message, unless suppressPass=true.
+			_tryToDo: function(fn, message, suppressPass)
+			{
+				suppressPass = suppressPass || false;
+				if (typeof fn === 'function')
+				{
+					try { 
+						fn();
+						if (!suppressPass) { this._addResult('pass', message,  fn.toString()); }
+					}
+					catch (e) {
+						this._addResult('fail', message, fn.toString(), e);
+						console.error(e)
 					}
 				}
-				this._tryToDo(setup, "Error during setup()", true);
-				tests.forEach(function(test, index) {
-					var message = descriptions[index];
-					this._tryToDo(test, message, false)
-				}.bind(this));
-				this._tryToDo(teardown, "Error during teardown()", true);
+			},
 
-				mndltest._end();
+			// add a result <li>
+			_addResult: function(result, description, functionToString, error)
+			{
+				var $testResult = $(document.createElement('li')).click(functionToString, this._displayTestFunction);
+				$testResult = $testResult.addClass(result).html(description);
+				if (error != undefined)
+				{
+					$(document.createElement('ul'))
+						.append($(document.createElement('li')).html(error.name + ': ' + error.message))
+						.appendTo($testResult);
+				}
+				if (result === 'fail') { this._$currentList.parents('div.test-group').addClass('fail').removeClass('pass'); }
+				this._$currentList.append($testResult);
+			},
+
+			// display results. 
+			_displayResults: function()
+			{
+				$(TEST_CONTAINER).show(200);
+			},
+
+			// displays a test case's test function
+			_displayTestFunction: function(e) 
+			{
+				e.stopPropagation();
+
+				if ($(this).data('preCreated'))	{ 
+					$(this).children('pre').toggle(300);
+				}
+
+				if ($(this).data('preCreated') === undefined)
+				{
+					var funcDisplayHandler = function(e) {
+						e.stopPropagation();
+						$(this).children('pre').hide(300);
+					}.bind(this);
+
+					$(this).data('preCreated', true);
+					$(document.createElement('pre'))
+					.html(e.data)
+					.click(funcDisplayHandler)
+					.hide()
+					.appendTo($(this))
+					.show(300);
+				}
+			},
+
+			dispatchEvent: function(type, properties, target)
+			{
+				var e = $.Event(type, properties);
+				$(target).trigger(e);
+			},
+
+			// run tests
+			run: function(testCases)
+			{
+				for (thisCase in testCases)
+				{
+					this._start(thisCase);
+					
+					var testCase = testCases[thisCase];
+					var fns = Object.keys(testCase);
+					var setup, teardown;
+					var tests = [];
+					var descriptions = [];
+					var i;
+					for (var i = 0; i < fns.length; i++)
+					{
+						if (fns[i] === 'setup') { setup = testCase[fns[i]]; }
+						else if (fns[i] === 'teardown') { teardown = testCase[fns[i]]; }
+						else {
+							descriptions.push(fns[i]);
+							tests.push(testCase[fns[i]]);
+						}
+					}
+					this._tryToDo(setup, "Error during setup()", true);
+					tests.forEach(function(test, index) {
+						var message = descriptions[index];
+						this._tryToDo(test, message, false)
+					}.bind(this));
+					this._tryToDo(teardown, "Error during teardown()", true);
+
+					this._end();
+				}
 			}
 		}
 	}
-})();
+
+	return mndltest;
+}
